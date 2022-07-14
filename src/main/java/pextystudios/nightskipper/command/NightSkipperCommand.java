35e1b3e643eceb8e6412d6eaf8b20c552137820a
TeaCondemns <tea.condemns@gmail.com>
2022-07-14 23:32:45 +0300

    Changes
    - Implementation of the functionality for the `/ns config value ...' command completed (added the ability to set the value)
    - Bug fix

--- src/main/java/pextystudios/nightskipper/command/NightSkipperCommand.java
+++ src/main/java/pextystudios/nightskipper/command/NightSkipperCommand.java
@@ -3,15 +3,16 @@ package pextystudios.nightskipper.command;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.Nullable;
 import pextystudios.nightskipper.NightSkipper;
-import pextystudios.nightskipper.util.LoggerUtil;
 import pextystudios.nightskipper.util.PlayerUtil;
 import pextystudios.nightskipper.util.SkipUtil;
 import pextystudios.nightskipper.util.SleepUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -63,8 +64,12 @@ public class NightSkipperCommand extends AbstractCommand {
         return stringList;
     }
 
+    private static int getWorldsListIndex(String key) {
+        return (int)Double.parseDouble(key.substring("feature.worlds-list.worlds".length() + 1, key.length() - 1));
+    }
+
     private static String getWorldsListValue(String key) {
-        return getWorldsList().get((int)Double.parseDouble(key.substring("feature.worlds-list.worlds".length() + 1, key.length() - 1)));
+        return getWorldsList().get(getWorldsListIndex(key));
     }
 
     @Override
@@ -113,16 +118,52 @@ public class NightSkipperCommand extends AbstractCommand {
                                 return;
                         }
 
-                    if (args.length >= 3 && args[1].equals("value") && getConfigKeys().contains(args[2])) {
+                    if (args.length >= 3 && args[1].equals("value")) {
+                        if (!getConfigKeys().contains(args[2])) {
+                            formatVars.put("key", args[2]);
+                            commandSender.sendMessage(NightSkipper.getText("invalid-config-key", formatVars));
+                            return;
+                        }
+
                         if (args.length == 3) {
                             String value;
-                            if (StringUtils.startsWith(args[2], "feature.worlds-list.worlds["))
+                            if (StringUtils.startsWith(args[2], "feature.worlds-list.worlds[")) {
                                 value = getWorldsListValue(args[2]);
+
+                                if (args[2].equals("feature.worlds-list.worlds[" + (getWorldsList().size() - 1) + "]")) {
+                                    commandSender.sendMessage(ChatColor.RED + "This value does not exist!");
+                                    return;
+                                }
+                            }
                             else if (StringUtils.startsWith(args[2], "feature.worlds-list.worlds"))
-                                value = NightSkipper.getInstance().getConfig().getList(args[2]).toString();
+                                value = NightSkipper.getInstance().getConfig().getStringList(args[2]).toString();
                             else value = NightSkipper.getInstance().getConfig().getString(args[2]);
 
                             commandSender.sendMessage(args[2] + ": " + value.replace("\n", "\\n"));
+                            return;
+                        } else {
+                            String newValue = String.join(" ", Arrays.copyOfRange(args, 3, args.length));
+                            String oldValue;
+
+                            if (args[2].startsWith("feature.worlds-list.worlds[")) {
+                                List<String> stringList = NightSkipper.getInstance().getConfig().getStringList("feature.worlds-list.worlds");
+                                int index = getWorldsListIndex(args[2]);
+                                stringList.set(index, newValue);
+                                oldValue = getWorldsListValue(args[2]);
+                            } else {
+                                oldValue = NightSkipper.getInstance().getConfig().getString(args[2]);
+
+                                if (Pattern.compile("^true|false$").matcher(newValue).find())
+                                    NightSkipper.getInstance().getConfig().set(args[2], newValue.equals("true"));
+                                else if (Pattern.compile("^\\d+$").matcher(newValue).find())
+                                    NightSkipper.getInstance().getConfig().set(args[2], (int)Double.parseDouble(newValue));
+                                else
+                                    NightSkipper.getInstance().getConfig().set(args[2], newValue.replace("\\\n", "\n"));
+                            }
+
+                            NightSkipper.getInstance().saveConfig();
+
+                            commandSender.sendMessage(ChatColor.GREEN + args[2] + ": " + oldValue.replace("\n", "\\\n") + " -> " + newValue);
                             return;
                         }
                     }
